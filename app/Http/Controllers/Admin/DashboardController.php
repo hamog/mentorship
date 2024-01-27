2f51b9f58c3878febbd4a3046e86c7ea3bdddbda
hamog <hashemm364@gmail.com>
2024-01-27 11:49:40 +0330

    Modify ;

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(): Renderable
     {
         return view('admin.dashboard.index');
     }
