43a8b64255de15cdbe4c9284663f7b77375e219c
hamog <hashemm364@gmail.com>
2024-01-31 14:47:31 +0330

    Add middleware in resource controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,9 +6,17 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('permission:view users', ['only' => ['index', 'show']]);
+        $this->middleware('permission:create users', ['only' => ['create', 'store']]);
+        $this->middleware('permission:edit users', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:delete users', ['only' => ['destroy']]);
+    }
+
     public function index()
     {
-        
+
     }
 
     public function create()
